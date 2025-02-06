eaf65a9c3933b36a849d084e0ae860e27905de76
Imperial-Marv <marcel_brard@outlook.com>
2025-02-06 13:20:25 -0300

    Your commit message

---
+++ next.config.ts next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add other configurations here
+  // reactStrictMode: true,
+  // basePath: '/myportfolio',
+};
+
+module.exports = nextConfig;
