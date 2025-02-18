3e537abdf01b0a3020e83f4a7c35e72c24f85734
Imperial-Marv <marcel_brard@outlook.com>
2025-02-18 16:05:55 -0300

    Trying again

--- next.config.ts next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  // Optional: Add other configurations here
-  // reactStrictMode: true,
-  // basePath: '/myportfolio',
-};
-
-module.exports = nextConfig;
